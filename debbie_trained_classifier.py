17d86a746b54c7c585f7363ee69d1657bc3cbac6
javi <javicorvi@gmail.com>
2020-06-15 12:08:36 +0200

    error with empty folder

--- debbie_trained_classifier.py
+++ debbie_trained_classifier.py
@@ -13,11 +13,6 @@ parser.add_argument('-o', help= 'paste path to folder of output folder')
 parser.add_argument('-w', help= 'paste path to workdir')
 args = parser.parse_args()
 
-#read input files into classifier 
-#test_set = [os.path.join(args.i, f) for f in os.listdir(args.i)]
-#read input files into classifier,  recursive and only txt files. 
-test_set = glob.glob(args.i + '/**/*.txt', recursive=True)
-
 #create output folder if not exist
 if not os.path.exists(args.o):
     os.makedirs(args.o)
@@ -46,9 +41,6 @@ def make_labels(data):
         docID = docID + 1
     return train_labels, file_list
 
-#make labels for test set
-test_set_labels, test_file_list = make_labels(test_set)
-
 #load trained model
 debbie_classifier = joblib.load(work_dir + 'svm_model.pkl')
 #load trained vectorizer
@@ -56,31 +48,61 @@ debbie_vectorizer = joblib.load(work_dir + 'count_vect.pkl')
 #load trained transformer
 debbie_transformer = joblib.load(work_dir + 'transformer.pkl')
 
-test_count = debbie_vectorizer.transform(test_set)
-test_tfidf = debbie_transformer.transform(test_count)
+#read input files into classifier 
+#test_set = [os.path.join(args.i, f) for f in os.listdir(args.i)]
+#read input files into classifier,  recursive and only txt files. 
 
-#fit the model
-predicted = debbie_classifier.predict(test_tfidf)
+#get subfolders that my contain txt files
+folders = glob.glob(args.i + '/**/', recursive=True)
+#add root in folders 
+folders.insert(0, args.i)
 
-# the results of the classification
-results = dict(zip(test_set, predicted))
-# df_results = pd.DataFrame.from_dict(results, orient='index')
-# df_results.to_csv("/Users/austinmckitrick/git/debbie/DEBBIE_DATA/debbie_classifier_results.csv", sep=',')
+total=0
+total_relevant=0
+total_non_relevant=0
 
-relevant_abstracts = []
-not_relevant_abstracts = []
-
-for key, value in results.items():
-    if value == 0.0:
-        relevant_abstracts.append(key)
-    elif value == 1.0:
-        not_relevant_abstracts.append(key)
-
-print('total number of abstracts collected', (len(relevant_abstracts)+len(not_relevant_abstracts)))
-print('number of relevant abstracts:', len(relevant_abstracts))
-print('number of non-relevant abstracts:', len(not_relevant_abstracts))
-
-#save relevant abstracts to folder
-for filename in test_set:
-    if str(filename) in relevant_abstracts:
-        copy2(filename, args.o)
+#iterate over folders and classify
+for f in folders:
+    print ('folder:' + f)
+    test_set = glob.glob(f + '/*.txt', recursive=True)
+    if(len(test_set)>0):
+        #make labels for test set
+        test_set_labels, test_file_list = make_labels(test_set)
+        
+        test_count = debbie_vectorizer.transform(test_set)
+        test_tfidf = debbie_transformer.transform(test_count)
+    
+        #fit the model
+        predicted = debbie_classifier.predict(test_tfidf)
+    
+        # the results of the classification
+        results = dict(zip(test_set, predicted))
+        # df_results = pd.DataFrame.from_dict(results, orient='index')
+        # df_results.to_csv("/Users/austinmckitrick/git/debbie/DEBBIE_DATA/debbie_classifier_results.csv", sep=',')
+    
+        relevant_abstracts = []
+        not_relevant_abstracts = []
+    
+        for key, value in results.items():
+            if value == 0.0:
+                relevant_abstracts.append(key)
+            elif value == 1.0:
+                not_relevant_abstracts.append(key)
+                
+        #save relevant abstracts to folder
+        for filename in test_set:
+            if str(filename) in relevant_abstracts:
+                copy2(filename, args.o)        
+        
+        print('total number of abstracts collected in folder', (len(relevant_abstracts)+len(not_relevant_abstracts)))
+        print('number of relevant abstracts in folder:', len(relevant_abstracts))
+        print('number of non-relevant abstracts in folder:', len(not_relevant_abstracts))    
+        total =  total  + len(relevant_abstracts)+len(not_relevant_abstracts)  
+        total_relevant =  total_relevant  + len(relevant_abstracts)  
+        total_non_relevant =  total_non_relevant  + len(not_relevant_abstracts)
+    else:
+        print('Emtpy Folder')
+print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print('total number of abstracts collected in folder', total)
+print('number of relevant abstracts in folder:', total_relevant)
+print('number of non-relevant abstracts in folder:', total_non_relevant)
